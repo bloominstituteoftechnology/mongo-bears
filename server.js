0b1fa305386557034a5e833cb79e186f7bdd1b28
mixelpixel <mixelpix@mac.com>
2017-08-14 18:30:27 -0400

    nodemon fix from upstream

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const Bears = require('./models.js)');
 
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
@@ -11,6 +12,20 @@ server.use(bodyParser.json());
 
 
 // TODO: write your server code here
+
+server.get('/bears', (req, res) => {
+  const { species, latinName } = req.body;
+  if (!species || !latinName) {
+    res.status(422);
+    res.json({ error: 'Please enter a SPECIES and a LATIN NAME' });
+    return;
+  }
+  const bear = new Bears({ species, latinName });
+  bear.save((err) => {
+    if (err) throw error;
+    res.json(bear);
+  })
+});
 
 
 mongoose.Promise = global.Promise;
