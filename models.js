ac05f699bb04da9c831f24e63d61c69089c2ba84
Dylan Scheidt <dys2@pitt.edu>
2017-09-18 15:05:47 -0700

    done

--- models.js
+++ models.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
-/* A schema is a description of the format of documents within a
- * collection. In this case, each Bear is a document of the form:
- *
- * {
- *   species: "American Black Bear",
- *   latinName: "Ursus americanus",
- *   createdAt: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT)
- * }
- *
- * Make createdAt default to the current date.
- */
 const BearSchema = new mongoose.Schema({
-  // TODO: write your schema here
+  species: {
+    type: String
+  },
+  latinName: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Bears', BearSchema);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-
+const Bears = require('./models.js');
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
 const server = express();
@@ -10,16 +10,46 @@ const server = express();
 server.use(bodyParser.json());
 
 
+server.post('/bears', (req, res) => {
+  const { species, latinName } = req.body;
+  if (!species || !latinName) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: "Missing species or latinName"});
+    return;
+  }
+  const bear = new Bears({species, latinName});
+  bear.save((err) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json({ error: "internal server error" });
+      return
+    }
+    res.json(bear);
+  });
+});
 
+server.get('/bears', (req, res) => {
+  Bears.find({}, (err, data) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json({ error: "Internal server error"});
+      return
+    }
+    res.json(data);
+  });
+});
 
-
-
-// TODO: write your server code here
-
-
-
-
-
+server.get('/bears/:id', (req, res) => {
+  const { id } = req.params;
+  Bears.findById(id, (err, bear) => {
+    if (err) {
+      req.status(STATUS_SERVER_ERROR);
+      res.json({ error: "Internal server error" });
+      return;
+    }
+    res.json(bear);
+  });
+});
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
