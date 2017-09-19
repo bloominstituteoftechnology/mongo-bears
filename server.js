6c54dfdd942bde38a473f549c84cbb26da89ff25
phytertek <ryan.phytertek@gmail.com>
2017-09-19 17:30:16 -0400

    mongo-I-mini complete

--- server.js
+++ server.js
@@ -9,17 +9,35 @@ const server = express();
 // allow server to parse JSON bodies from POST/PUT/DELETE requests
 server.use(bodyParser.json());
 
+// TODO: DONE - write your server code here
 
+// get Bear model
+const Bear = require('./models');
 
+// POST /bears -- accepts bear
+server.post('/bears', (req, res) => {
+  const newBear = new Bear(req.body);
+  newBear.save()
+    .catch(error => res.status(500).json({ error }))
+    .then(result => res.json(result));
+});
 
+// GET /bears -- returns bears object
+server.get('/bears', (req, res) => {
+  Bear.find()
+    .catch(error => res.status(500).json({ error }))
+    .then(bears => res.json(bears.reduce((obj, bear) => {
+      obj[bear.id] = bear;
+      return obj;
+    }, {})));
+});
 
-
-// TODO: write your server code here
-
-
-
-
-
+// GET /bears/:id -- expect id parameter on url, return bear
+server.get('/bears/:id', (req, res) => {
+  Bear.findById(req.params.id)
+    .catch(error => res.status(500).json({ error }))
+    .then(bear => res.json(bear));
+});
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
