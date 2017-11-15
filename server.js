b8255ddb9fdbd01c9994d2aadafb12e721a34306
Sam Kim <sams404link@gmail.com>
2017-11-15 15:08:49 -0800

    mini-sprint updated

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
 const server = express();
+const Bear = require('./BearModel.js')
 
 // allow server to parse JSON bodies from POST/PUT/DELETE requests
 server.use(bodyParser.json());
@@ -15,8 +16,38 @@ server.use(bodyParser.json());
 
 
 // TODO: write your server code here
+server.post('/api/bears', function (req, res) {
+  const newBear = new Bear(req.body);
+  
+  newBear.save(function(err, bear) {
+    if(err) {
+      res.status(STATUS_SERVER_ERROR).json({ error: 'Some useful error message '});
+    } else {
+      res.status(200).json(bear);
+    }
+  });
+});
 
+server.get('/api/bears', function(req, res) {
+  Bear.find({}, function(err, bears) {
+    if(err) {
+      res.status(404);
+    } else {
+      res.status(200).json(bears);
+    }
+  });
+});
 
+server.get('/api/bears/:id', function(res, req) {
+  const { id } = req.params;
+  Bear.findById({ _id: id }, function(err, bears) {
+    if(err) {
+      res.status(404);
+    } else {
+      res.status(200).json(bears);
+    }
+  });
+});
 
 
 
