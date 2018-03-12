2fe59095c3e2f6d453ba2002da16e8638247b376
Jonathan Matthew Brunt <jonbrunt@Jonathans-MBP.hsd1.tx.comcast.net>
2018-03-12 14:59:28 -0500

    Schema and Model
    in BearModel.js:
    create BearSchema
    create BearModel
    export model

---
+++ Bears/BearModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const BearSchema = new mongoose.Schema({
+  species: {
+    type: String,
+    required: true,
+  },
+  latinName: {
+    type: String,
+    required: true,
+  },
+  createdOn: {
+    type: Date,
+    required: true,
+  }
+});
+
+const BearModel = mongoose.model('Bear', BearSchema);
+
+module.exports = BearModel;
