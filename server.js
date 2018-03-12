e0c49d39171e480b6d21d845590c688edb9d2e54
TERR1E <terrieb.kim@gmail.com>
2018-03-12 14:06:53 -0700

    fixed server.js, get working, testing post

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors'); // https://www.npmjs.com/package/cors
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const bearRouter = require('./BearKeeper/bearRoutes');
+const Bear = require('./BearKeeper/BearModel');
 
 const server = express();
 
@@ -18,15 +18,51 @@ server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
 
-server.use('/bearkeeper', bearRouter);
+// server.use('/bears', bearRouter);
+server.post('/api/bears', function(req, res) {
+  const {
+    bearInfo,
+    species,
+    latinName
+   } = req.body;
+
+  const bear = new Bear(bearInfo);
+
+  
+  if (!species || !latinName) {
+    console.log(err);
+    res.status(400).json({ error: 'Please provide both species and latinNAme for the Bear.' });
+  } else {
+    bear
+      .save()
+      .then(savedBear => {
+        res.status(201).json(savedBear);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: 'There was an error while saving the Bear to the Database' });    
+      });
+  }
+});
+
+server.get('/api/bears', function(req, res) {
+  Bear.find({})
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err => {
+      consolelog(err);
+      res.status(500).json({ error: 'The information could not be retrieved.' });
+    });
+});
 
 mongoose
-  .connect('mongodb://localhost/store')
+  .connect('mongodb://localhost/BearKeeper')
   .then(connected => {
     console.log('Successfully Connected to MongoDB');
   })
   .catch(err => {
-    console.log('Database connecttion failed');
+    console.log('Database connection failed');
   });
 
 
