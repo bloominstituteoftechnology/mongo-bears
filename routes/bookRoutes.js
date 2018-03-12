d262dd1729756910d5c0f008170457a9c07175f9
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-12 17:59:10 -0400

    code cleanup and injected model

---
+++ routes/bookRoutes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const routes = (Bear) => {
+  const bearRouter = express.Router();
+
+  bearRouter.route('/bears')
+    .get((req, res) => {
+      Bear.find({})
+      .then(bears => {
+        res.status(200).json(bears);
+      })
+      .catch(err => {
+        res.status(500).json({ err: 'Error creating bear' });
+      })
+    })
+    .post((req, res) => {
+      const bear = new Bear(req.body);
+      bear.save().then(savedBear => {
+        res.status(201).json(savedBear);
+      }).catch(err => {
+        res.status(500).json({ err: 'Error creating bear' });
+      });
+    });
+
+  bearRouter.route('/bears/:id')
+    .get((req, res) => {
+      Bear.findById(req.params.id)
+        .then(id => {
+          res.status(200).json(id);
+        })
+        .catch(err => {
+          res.status(500).json('Error finding bear');
+        });
+    });
+    return bearRouter;
+};
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -9,42 +9,12 @@ const db = mongoose.connect('mongodb://localhost/BearKeeper');
 
 const Bear = require('./models/bearModel');
 
-const bearRouter = express.Router();
-
 app.use(helmet()); // https://helmetjs.github.io/
 app.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-bearRouter.route('/bears')
-  .get((req, res) => {
-    Bear.find({})
-    .then(bears => {
-      res.status(200).json(bears);
-    })
-    .catch(err => {
-      res.status(500).json({ err: 'Error creating bear' });
-    })
-  })
-  .post((req, res) => {
-    const bear = new Bear(req.body);
-    bear.save().then(savedBear => {
-      res.status(201).json(savedBear);
-    }).catch(err => {
-      res.status(500).json({ err: 'Error creating bear' });
-    });
-  });
-
-bearRouter.route('/bears/:id')
-  .get((req, res) => {
-    Bear.findById(req.params.id)
-      .then(id => {
-        res.status(200).json(id);
-      })
-      .catch(err => {
-        res.status(500).json('Error finding bear');
-      });
-  });
+const bearRouter = require('./routes/bookRoutes')(Bear);
 
 app.use('/api', bearRouter);
 
