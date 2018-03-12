9b6dfb605fac52f918c5da2aa9260271ed7751cd
TERR1E <terrieb.kim@gmail.com>
2018-03-12 13:49:07 -0700

    fixing merging

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
-// import { Mongoose } from 'mongoose';
-
 const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors'); // https://www.npmjs.com/package/cors
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const bearRouter = require('./BearKeeper/bearRoutes');
-
-const server = express();
-
-server.use(helmet()); // https://helmetjs.github.io/
-server.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
-server.use(bodyParser.json());
-
-server.get('/', function(req, res) {
-  res.status(200).json({ status: 'API Running' });
+const Bear = require('./BearModel.js');
+const bearsRouter = express.Router();
+bearsRouter.post('/api/bears', function(req, res) {
+  const bearInfo = req.body;
+  const bear = new Bear(bearInfo);
+  bear
+    .save()
+    .then(savedBear => {
+      res.status(201).json(savedBear);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({ error: 'Please provide both species and latinNAme for the Bear.' });
+    });
 });
-
-server.use('/bearkeeper', bearRouter);
-
-mongoose
-  .connect('mongodb://localhost/store')
-  .then(connected => {
-    console.log('Successfully Connected to MongoDB');
-  })
-  .catch(err => {
-    console.log('Database connecttion failed');
-  });
-
-
-const port = process.env.PORT || 5005;
-server.listen(port, () => {
-  console.log(`API running on http://localhost:${port}.`);
+bearsRouter.get('/api/bears', function(req, res) {
+  Bear.find({})
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err => {
+      consolelog(err);
+      res.status(500).json({ error: 'The information could not be retrieved.' });
+    });
 });
+module.exports = bearsRouter;
