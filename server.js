d860f35d12d0840c0763ba5e5d9bffd0458018bf
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-12 16:59:39 -0400

    Connect to MongoDB and Begin Creating API Endpoints
    - Installed mongoose to project
    - Import required dependencies into server and model files
    - Created model file to create Schema for Bears collection
    - Began creating API Endpoints

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors'); // https://www.npmjs.com/package/cors
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Bear = require('./BearModel.js'); 
 
 const server = express();
 
@@ -13,7 +16,76 @@ server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
 
-const port = process.env.PORT || 5005;
-server.listen(port, () => {
+
+server.post('/api/bears', (req, res) => {
+  const { species, latinName, createdOn } = req.body;
+  if (!species || !latinName) {
+    res.status(400).json({ errorMessage: "Please provide both species and latinName for the Bear."});
+    return;
+  }
+  const bear = new Bear({ species, latinName, createdOn });
+  bear.save()
+  .then(savedBear => {
+    res.status(201).json(savedBear);
+  })
+  .catch(err => {
+    res.status(500).json({ errorMessage: "There was an error while saving the Bear to the Database"})
+  })
+})
+
+server.get('/api/bears', (req, res) => {
+  Bear.find({})
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err => {
+      res.statu(500).json({ error: "The information could not be retrieved."})
+    })
+})
+
+server.get('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  if (!Bear.find({_id: id})) {
+    res.status(404).json({ message: "The Bear with the specified ID does not exist."})
+  }
+  Bear.findById(id, (err, bear) => {
+    if (err) {
+      res.status(500).json({ message: "The information could not be retreived."})
+    }
+    res.json(bear);
+  })
+})
+
+server.delete('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  Bear.findByIdAndRemove(id, (err) => {
+    if (err) {
+      res.status(500).json({ error: "The Bear could not be removed" })
+    }
+    res.json({ success: "Successfully removed bear"});
+  })
+})
+
+server.put('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  Bear.findByIdAndUpdate(id, {$set: req.body}, (err, bear) => {
+    if (err) {
+      res.status(500).json({ error: "The Bear information could not be modified" })
+    }
+    res.json({ success: "The Bear information was successfully modified"});
+  })
+})
+
+  mongoose.connect('mongodb://localhost/store')
+    .then(conn => {
+      console.log('Successfully Connected to MongoDB');
+    })
+    .catch(err => {
+      console.log('Database connection failed');
+    }
+  );
+
+  const port = process.env.PORT || 5005;
+  server.listen(port, () => {
   console.log(`API running on http://localhost:${port}.`);
 });
