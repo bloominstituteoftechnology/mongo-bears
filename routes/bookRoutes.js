7362147478986b3caedc4595ecca4103587457de
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-12 18:50:42 -0400

    implemented delete

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -10,7 +10,7 @@ const routes = (Bear) => {
         res.status(200).json(bears);
       })
       .catch(err => {
-        res.status(500).json({ err: 'Error creating bear' });
+        res.status(500).json({ error: 'The information could not be retrieved.' });
       })
     })
     .post((req, res) => {
@@ -18,19 +18,27 @@ const routes = (Bear) => {
       bear.save().then(savedBear => {
         res.status(201).json(savedBear);
       }).catch(err => {
-        res.status(500).json({ err: 'Error creating bear' });
+        res.status(500).json({ error: 'Error creating bear' });
       });
     });
+  bearRouter.use('/bears/:id', (req, res, next) => {
+    Bear.findById(req.params.id)
+      .then(bear => {
+        if (bear) {
+          req.bear = bear;
+          next();
+        } else {
+          res.status(404).json({ message: 'The Bear with the specified ID does not exist.' });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'The information could not be retrieved.' });
+      });
+  });
 
   bearRouter.route('/bears/:id')
     .get((req, res) => {
-      Bear.findById(req.params.id)
-        .then(id => {
-          res.status(200).json(id);
-        })
-        .catch(err => {
-          res.status(500).json('Error finding bear');
-        });
+      res.json(req.bear);
     })
     .put((req, res) => {
       Bear.findById(req.params.id)
@@ -42,8 +50,23 @@ const routes = (Bear) => {
           });
         })
         .catch(err => {
-          res.status(500).json('Error finding bear');
+          res.status(500).json({ error: 'The information could not be retrieved.' });
         })
+    })
+    .delete((req, res) => {
+      Bear.findById(req.params.id)
+        .then(bear => {
+          bear.remove()
+            .then(() => {
+              res.status(204).json('Removed');
+            })
+            .catch(err => {
+              res.status(500).json({ error: 'The Bear could not be removed' });
+            });
+        })
+        .catch(err => {
+          res.status(500).json({ error: 'The Bear could not be removed' });
+        });
     });
     return bearRouter;
 };
