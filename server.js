62efad378711706be7c230d8eee9a916fe0a7eb1
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-12 16:24:27 -0400

    implemented HTTP get

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(helmet()); // https://helmetjs.github.io/
 app.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
 app.use(bodyParser.json());
 
+bearRouter.route('/bears')
+  .get((req, res) => {
+    const responseJSON = { hello: 'This is my API' };
+    res.json(responseJSON);
+  });
+
 app.use('/api', bearRouter);
 
 app.get('/', function(req, res) {
