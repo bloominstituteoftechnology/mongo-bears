4cd6869b88b75fd91be579e39d10fda8d2105767
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-12 14:45:06 -0500

    Mongoose
    require mongoose
    connect to mongoose
    connect to local BearKeeper db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors'); // https://www.npmjs.com/package/cors
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const server = express();
 
@@ -12,6 +13,15 @@ server.use(bodyParser.json());
 server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
+
+mongoose
+  .connect('mongodb://localhost/BearKeeper')
+  .then(conn => {
+    console.log('Successfully Connected to MongoDB');
+  })
+  .catch(err => {
+    console.log('Database connection failed');
+  });
 
 const port = process.env.PORT || 5005;
 server.listen(port, () => {
