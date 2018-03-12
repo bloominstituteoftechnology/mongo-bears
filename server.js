38447e02884359e4dd7f1f34744d8bc6f23b8685
aceredesign <steven.magadan@hotmail.com>
2018-03-12 14:15:25 -0700

    got get request and post

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors'); // https://www.npmjs.com/package/cors
-const bodyParser = require('body-parser');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors"); // https://www.npmjs.com/package/cors
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+const Bear = require("./bears/Bears.js");
 const server = express();
 
 server.use(helmet()); // https://helmetjs.github.io/
-server.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
+server.use(cors()); // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
 server.use(bodyParser.json());
 
-server.get('/', function(req, res) {
-  res.status(200).json({ status: 'API Running' });
+mongoose
+  .connect("mongodb://localhost/BearSchema")
+  .then(db => {
+    console.log("Connected to mongo");
+  })
+  .catch(error => {
+    console.log("Error");
+  });
+
+server.get("/", function(req, res) {
+  res.status(200).json({ status: "API Running" });
+});
+
+server.post("/api/bears", (req, res) => {
+  const bearBody = req.body;
+  const { species, latinName } = bearBody;
+  const bear = new Bear(bearBody);
+  bear
+    .save()
+    .then(savedBear => {
+      res.status(201).json(savedBear);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "Please put both species and latinName for the bear"
+      });
+    });
+});
+
+server.get("/api/bears", (req, res) => {
+  Bear.find({})
+    .then(bear => {
+      res.status(200).json(bear);
+    })
+    .catch(error => {
+      res.status(500).json({ error: error });
+    });
 });
 
 const port = process.env.PORT || 5005;
