5527a2371e4ce56e231be02b344bca19a7f9aa96
Dixie Korley <dixiekorley@gmail.com>
2018-03-12 18:32:34 -0400

    DELETE by Id working

--- bears/BearKeeperRoutes.js
+++ bears/BearKeeperRoutes.js
@@ -46,16 +46,21 @@ bearsRouter.post('/', (req, res) => {
          })
  });
 
-// bearsRouter.delete('/api/bears/:id', (req, res) => {
-//     const bearInfo = req.body;
-//     const bear = new Bear(bearInfo);
+bearsRouter.delete('/:id', (req, res) => {
+    const { id } = req.params;
 
-//     //Remove bear
-//     .then()
-//     .catch(err => {
-//         res.status(500).json({ message: `error deleting the bear: ${err}`})
-//     })
-// });
+    BearKeeper.findByIdAndRemove(id)
+
+    .then(bear => {
+        if (!bear) {
+            res.status(404).json({ message: `The Bear with the specified ID does not exist.` });
+        }
+        res.status(200).json(bear);
+    })
+    .catch(err => {
+        res.status(500).json({ error: `The Bear could not be removed.` });
+    });
+});
 
 // bearsRouter.put('/api/bear s/:id', (req, res) => {
 //     const bearInfo = req.body;
