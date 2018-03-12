070846bd5dec311e3242e980d6ab7d51b727f52f
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-12 16:15:38 -0400

    created app router

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const helmet = require('helmet');
 const cors = require('cors'); // https://www.npmjs.com/package/cors
 const bodyParser = require('body-parser');
 
-const server = express();
+const app = express();
 
-server.use(helmet()); // https://helmetjs.github.io/
-server.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
-server.use(bodyParser.json());
+const bearRouter = express.Router();
 
-server.get('/', function(req, res) {
+app.use(helmet()); // https://helmetjs.github.io/
+app.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
+app.use(bodyParser.json());
+
+app.use('/api', bearRouter);
+
+app.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
 
 const port = process.env.PORT || 5005;
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`API running on http://localhost:${port}.`);
 });
