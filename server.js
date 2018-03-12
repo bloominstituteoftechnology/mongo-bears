d04fa976b6cb41190b000eba0c1957ce95ccbaf7
Michael Marshal <mikemarshalman@gmail.com>
2018-03-12 17:39:25 -0500

    bear by id

--- server.js
+++ server.js
@@ -55,6 +55,31 @@ server.get('/api/bears', (req, res) => {
         .json({ error: 'Couldnt get em'});
     });
 });
+// get bear by id
+server.get('/api/bears/:id', (req, res) => {
+  const id = req.params.id;
+  Bear.findById(id)
+    .then(bear => {
+      if (bear) {
+        res
+          .status(200)
+          .json(bear);
+      } else {
+        res.status(404).json({ message: 'Bear with ID not found' });
+      }
+    })
+    .catch(error => {
+      if (error.name === 'CastError') {
+        res
+          .status(400)
+          .json({ message: `Invalid ID: ${error.value}` });
+      } else {
+        res
+          .status(500)
+          .json({ error: 'Couldnt get the bear info'});
+      }
+    });
+});
 
 mongoose
   .connect('mongodb://localhost/BearKeeper')
