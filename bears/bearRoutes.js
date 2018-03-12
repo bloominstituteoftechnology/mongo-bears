25f8634a8dc72a9cb27954ddb336764b66c1f236
cliffkang <cliffgkang@gmail.com>
2018-03-12 13:30:05 -0700

    finished setting up the boilerplate

---
+++ bears/bearRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const Bear = require("./BearModel.js");
+
+const bearsRouter = express.Router();
+
+const STATUS_SUCCESS = 200;
+const STATUS_CREATED = 201;
+const STATUS_BAD_REQUEST = 400;
+const STATUS_USER_ERROR = 500;
+
+bearsRouter.post("/", (req, res) => {
+  const bearInfo = req.body;
+
+  const bear = new Bear(bearInfo);
+
+  bear
+    .save()
+    .then(savedBear => {
+      res.status(STATUS_CREATED);
+      res.send(savedBear);
+    })
+    .catch(err => {
+      // console.log('save error is', err);
+      res.status(STATUS_USER_ERROR);
+      res.send({ error: "There was an error while saving the Bear to the Database" });
+    });
+});
+
+module.exports = bearsRouter;
