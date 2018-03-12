17da6ba2e7569e1252a77600b35595a90200df57
TERR1E <terrieb.kim@gmail.com>
2018-03-12 13:22:03 -0700

    set up schema, model, routes for get & post

---
+++ BearKeeper/bearRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const Bear = require('./BearModel.js');
+
+const bearsRouter = express.Router();
+
+bearsRouter.post('/api/bears', function(req, res) {
+  const bearInfo = req.body;
+
+  const bear = new Bear(bearInfo);
+
+  bear
+    .save()
+    .then(savedBear => {
+      res.status(201).json(savedBear);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({ error: 'Please provide both species and latinNAme for the Bear.' });
+    });
+});
+
+bearsRouter.get('/api/bears', function(req, res) {
+  Bear.find({})
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err => {
+      consolelog(err);
+      res.status(500).json({ error: 'The information could not be retrieved.' });
+    });
+});
+
+module.exports = bearsRouter;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// import { Mongoose } from 'mongoose';
+
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors'); // https://www.npmjs.com/package/cors
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const bearRouter = require('./BearKeeper/bearRoutes');
 
 const server = express();
 
@@ -12,6 +17,18 @@ server.use(bodyParser.json());
 server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
+
+server.use('/bearkeeper', bearRouter);
+
+mongoose
+  .connect('mongodb://localhost/store')
+  .then(connected => {
+    console.log('Successfully Connected to MongoDB');
+  })
+  .catch(err => {
+    console.log('Database connecttion failed');
+  });
+
 
 const port = process.env.PORT || 5005;
 server.listen(port, () => {
