8e5c7410f87c8f0ba337ae00e0b85f4c0bfb66d3
Christopher Beards <beardscm@gmail.com>
2018-03-12 15:34:42 -0400

    Successfully Connected to MongoDB

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors'); // https://www.npmjs.com/package/cors
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const server = express();
 
@@ -9,9 +10,22 @@ server.use(helmet()); // https://helmetjs.github.io/
 server.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
 server.use(bodyParser.json());
 
+
 server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
+
+
+mongoose
+  .connect('mongodb://localhost/BearKeeper')
+  .then(conn => {
+    console.log('Successfully Connected to MongoDB');
+  })
+  .catch(err => {
+    console.log('Database connection failed');
+  });
+
+
 
 const port = process.env.PORT || 5005;
 server.listen(port, () => {
