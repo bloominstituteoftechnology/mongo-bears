d119a0177f4faacc914f3da3524482abc280d229
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-13 05:19:40 -0400

    formatted date

--- models/bearModel.js
+++ models/bearModel.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
+const date = new Date();
+
 const BearSchema = new mongoose.Schema({
   species: { type: String, required: true },
   latinName: { type: String, required: true },
-  createdOn: { type: Date, default: Date.now }
+  createdOn: {
+    type: Date,
+    default: date.toDateString() + ' ' + date.toTimeString()
+  }
 });
 
 const BearModel = mongoose.model('Bear', BearSchema);
