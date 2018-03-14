22b4ce7e01b76a40612aeb0ce187add58a7e1535
Charlie Sparks <csparks@antioch.edu>
2018-03-14 15:10:47 -0700

    post to /api/bears complete

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const BearSchema = require("./models/Bears");
 
 const server = express();
 
+mongoose.Promise = global.Promise;
+
 server.use(helmet()); // https://helmetjs.github.io/
 server.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
 server.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/27017/api/bears");
+mongoose.connect("mongodb://localhost:27017/BearKeeper");
 mongoose.connection
   .once("open", () => console.log(`Successfully connected to MongoDB.`))
   .on("error", (err) => console.log(`Database connection failed.`));
@@ -19,6 +21,24 @@ mongoose.connection
 server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
+
+server.post("/api/bears", (req, res) => {
+  const bearSpecies = req.body.species;
+  const bearLatin = req.body.latinName;
+  const timeStamp = new Date();
+  const newBear = new BearSchema({
+    species: bearSpecies,
+    latinName: bearLatin,
+    createdOn: timeStamp,
+  })
+
+  newBear.save()
+  .then(response => res.send(`This bear was added: ${response}`))
+  .catch(err => {
+    console.log(`There was an error: ${err}`);
+    res.send(`There was an error!`)
+  })
+})
 
 const port = process.env.PORT || 5005;
 server.listen(port, () => {
