138fe34690aa3860e0fd7c1a41f10457fdafd1c6
KJ Sourcing <tmkogwork@gmail.com>
2018-04-10 14:50:26 -0700

    mini complete

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,24 +1,86 @@
 const router = require('express').Router();
 
+const Bear = require('./bearModel');
+
+// /api/bears
 router
   .route('/')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
+    Bear.find({})
+      .then(bears => {
+        res.status(200).json(bears);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+    const bear = new Bear(req.body);
+
+    bear
+      .save()
+      .then(savedBear => {
+        // change the saved bear
+        res.status(201).json(savedBear);
+      })
+      .catch(err => res.status(500).json(err));
   });
 
 router
   .route('/:id')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' + req.params.id });
+    Bear.findById(req.params.id)
+      .then(bears => {
+        res.status(200).json(bears);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
   })
   .delete((req, res) => {
-    res.status(200).json({ status: 'please implement DELETE functionality' });
+    const { id } = req.params;
+    Bear.findByIdAndRemove(id)
+      .then(response => {
+        if (response === null) {
+          res.status(404).json({ message: 'not found' });
+        } else {
+          res.status(200).json(response);
+        }
+      })
+      .catch(err => {
+        if (err.name === 'CastError') {
+          res.status(400).json({
+            message: 'The id provided is invalid, please check and try again.',
+          });
+        } else {
+          res
+            .status(500)
+            .json({ errorMessage: 'The friend could not be removed', err });
+        }
+      });
   })
   .put((req, res) => {
-    res.status(200).json({ status: 'please implement PUT functionality' });
+    // const changes = { ...req.body, updatedOn:new Date() }
+
+    Bear.findByIdAndUpdate(req.params.id, req.body)
+      .then(response => {
+        if (response === null) {
+          res.status(404).json({ message: 'not found' });
+        } else {
+          res.status(200).json(response);
+        }
+      })
+      .catch(err => {
+        if (err.name === 'CastError') {
+          res.status(400).json({
+            message: 'The id provided is invalid, please check and try again.',
+          });
+        } else {
+          res
+            .status(500)
+            .json({ errorMessage: 'The friend could not be removed', err });
+        }
+      });
   });
 
 module.exports = router;
