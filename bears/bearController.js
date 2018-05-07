979d7660b111cd27e7e3b4325eda3d0bb5dd0a9b
Justin M <jekm321@gmail.com>
2018-05-07 18:57:52 -0400

    finished crud functions on the mini proj

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,24 +1,69 @@
 const router = require('express').Router();
 
+const Bear = require('./bearModel');
+
 router
   .route('/')
-  .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
-  })
-  .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
-  });
+  .get(get)
+  .post(post);
 
 router
   .route('/:id')
-  .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' + req.params.id });
-  })
-  .delete((req, res) => {
-    res.status(200).json({ status: 'please implement DELETE functionality' });
-  })
-  .put((req, res) => {
-    res.status(200).json({ status: 'please implement PUT functionality' });
+  .get(getById)
+  .delete(destroy)
+  .put(put);
+
+function get(req, res) {
+  Bear.find().then(bears => {
+    res.status(200).json(bears);
   });
+}
+
+function post(req, res) {
+  const bearData = req.body;
+
+  const bear = new Bear(bearData);
+
+  bear
+    .save()
+    .then(bear => {
+      res.status(201).json(bear);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+}
+
+function getById(req, res) {
+  const { id } = req.params;
+
+  Bear.findById()
+    .then(bear => {
+      res.status(200).json(bear);
+    });
+}
+
+function put(req, res) {
+  const { id } = req.params;
+  const newInfo = req.body;
+  
+  Bear.findyByIdAndUpdate(id, newInfo)
+    .then(response => {
+      Bear.find().then( bears => {
+        res.status(200).json(bears);
+      });
+    })
+}
+
+function destroy(req, res) {
+  const { id } = req.params;
+  
+  Bear.findyByIdAndRemove(id)
+    .then(response => {
+      Bear.find().then( bears => {
+        res.status(200).json(bears);
+      });
+    })
+}
 
 module.exports = router;
