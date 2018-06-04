2eed363611387775ef0545de02c19a6446ac3444
NandoTheessen <nando.theessen@gmail.com>
2018-06-04 20:49:27 +0200

    Added Crud functionality, implemented mongoose, implemented Controller, implemented Model

---
+++ bears/bearModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+//SCHEMA
+
+// {
+//     species: 'Grizlly Bear',
+//         latinName: "blkabla",
+//             createdOn: Date.now();
+// }
+
+const BearSchema = new mongoose.Schema({
+    species: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    latinName: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const bearsModel = mongoose.model('Bear', BearSchema)
+
+module.exports = bearsModel;
