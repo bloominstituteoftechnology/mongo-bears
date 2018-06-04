215c6de11cc116232918e2307351f8e22012eca6
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-04 15:59:56 -0400

    edit

--- bears/bearModel.js
+++ bears/bearModel.js
@@ -1,24 +1,21 @@
-// create a model for our bears.
+
 const mongoose = require('mongoose');
 
 const BearSchema = new mongoose.Schema({
-  // declare a schema. Your documents will 'look like this'
   species: {
-    type: String, // There are many types in Mongoose that we'll learn about this week
-    required: true, // required is a validator. It tells us that this field is required
-    unique: true // unique is also a validator. It tells us this field can only exist once
+    type: String, 
+    required: true,
+    unique: true 
   },
   latinName: {
     type: String,
     require: true
   },
   createOn: {
-    type: Date, // Date is a nother type in Mongoose.
+    type: Date,
     default: Date.now()
   }
 });
 
-const bearsModel = mongoose.model('Bear', BearSchema); // this is where we declare this as a model.
-// by passing our BearSchema to this model we decalre that it will be a collection in our DB
-
+const bearsModel = mongoose.model('Bear', BearSchema); 
 module.exports = bearsModel;
