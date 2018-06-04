79fc47a0492cdc2155934e48875bface2e1a0bc8
kammagik <kammagik@yahoo.com>
2018-06-04 13:13:13 -0700

    initail commit

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ server.use('/api/bears', bearController);
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n=== API running on http://localhost:${port} ===\n`);
+  console.log(`\n=== Successfully Connected to MongoDB ===\n`);
 });
