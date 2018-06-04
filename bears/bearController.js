9c2a2f2226e82fd14401e094005df9ab87d482ec
Derrick-Mei <derrick.mei.169@gmail.com>
2018-06-04 17:16:41 -0500

    cleaned up code

--- bears/bearController.js
+++ bears/bearController.js
@@ -44,31 +44,6 @@ router
         });
       });
   });
-
-// router
-//   .route("/:id")
-//   .get((req, res) => {
-//     //res.status(200).json({ route: "/api/bears/" + req.params.id });
-//     const { id } = req.params;
-//     Bear.findById(id)
-//       .then(bearArray => {
-//         console.log("bearArray: ", bearArray);
-//       })
-//       .catch(err => console.log("err: ", err));
-//   })
-// router
-//   .route("/:id")
-//   .get((req, res) => {
-//     const { id } = req.params;
-//     Bear.findById(id)
-//       .then(foundBear => {
-//         console.log("nobear: ", foundBear);
-//         res.status(200).json(foundBear);
-//       })
-//       .catch(err => {
-//         res.status(404).json({ error: "No bear by that id in DB" });
-//       });
-//   })
 router
   .route("/:id")
   .get((req, res) => {
