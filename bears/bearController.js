0995d26b052cda48eefb39bc808285320116a178
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-05 05:55:04 +0700

    cleaned up code

--- bears/bearController.js
+++ bears/bearController.js
@@ -8,16 +8,14 @@ router
     Bear.find()
       .then(bears => {
         res.status(200).json(bears);
-        return;
       })
-      .catch(err => res.status(500).json({ error: "Something went terribly wrong!" }));
+      .catch(err => res.status(500).json({ error: "The bear information could not be retrieved." }));
   })
   .post((req, res) => {
     const { species, latinName } = req.body;
     const newBear = new Bear({ species, latinName });
     if (!species || !latinName) {
       res.status(400).json({ error: "Please provide both species and latinName for the bear." });
-      return;
     };
     newBear
       .save()
@@ -36,7 +34,6 @@ router
     Bear.findById(id)
       .then(bear => {
         res.status(200).json(bear);
-        return;
       })
       .catch(err => {
         res.status(404).json({ error: "Bear cannot be found with given ID." });
@@ -47,7 +44,6 @@ router
     Bear.findByIdAndRemove(id)
       .then(bear => {
         res.json(bear);
-        return;
       })
       .catch(err => {
         res.status(404).json({ error: "Bear was not found with given ID." });
@@ -61,7 +57,6 @@ router
     Bear.findByIdAndUpdate(id, updatedBear)
       .then(update => {
         res.json(updatedBear);
-        return;
       })
       .catch(err => {
         res.status(404).json({ error: "Bear was not found with given ID." });
