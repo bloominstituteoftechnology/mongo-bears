f00b31d19e3f7f78ff7b775879333ec29a6ff9d8
A99 <A99@99s-macbook-pro.home>
2018-06-05 00:52:08 +0200

    Initial Commit / Not functioning

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const bearController = require('./bears/bearController');
-
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use('/api/bears', bearController);
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:5000/beardb', {}, err => {
+  if(err) console.log(err);
+  console.log(`Mongoose is connected to our DB.`);
+});
 
 server.get('/', function(req, res) {
   res.status(200).json({ api: 'running' });
