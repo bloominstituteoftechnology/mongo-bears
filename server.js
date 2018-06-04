8b43eb6f6774e3bda18915cba32105e946635308
Ryan Hamblin <ryanhca@gmail.com>
2018-06-04 11:59:04 -0600

    partial solution up to delete and put

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const bearController = require('./bears/bearController');
 
@@ -17,6 +18,13 @@ server.get('/', function(req, res) {
 server.use('/api/bears', bearController);
 
 const port = process.env.PORT || 5000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/dbBears', {}, err => {
+  if (err) console.log(err);
+  console.log('Mongoose connected us to our DB');
+});
+
 server.listen(port, () => {
   console.log(`\n=== API running on http://localhost:${port} ===\n`);
 });
