b53078bad8c88803189450dbb51cf57373460ff9
udykas <udykas@gmail.com>
2018-06-04 17:49:17 -0400

    Created the bear model

---
+++ bears/bearModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const definition = {
+    species: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    latinName: {
+        type: String,
+        require: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now()
+    }
+}
+
+const options = {
+    timestamps: true
+}
+
+const bearSchema = new mongoose.Schema(definition, options)
+
+const bearModel = mongoose.model('Bear', bearSchema)
