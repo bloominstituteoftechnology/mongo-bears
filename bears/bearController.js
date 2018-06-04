ddb2277bb23ecccb8c817efa392cf3e26ac8eb45
Xang Thao <xangthao24@yahoo.com>
2018-06-04 13:00:12 -0700

    Able to create and show all bears

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,12 +1,31 @@
 const router = require('express').Router();
+const Bear = require("./bearModel");
+
+let errorMessage = (statusCode, message, res) => {
+  res.status(statusCode).json({err: message})
+};
 
 router
   .route('/')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
+    Bear.find()
+      .then(bears => {
+        res.status(200).json(bears);
+      })
+      .catch(err => {
+        errorMessage(500, "Error fetching bears", res);
+      })
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+    const { species, latinName } = req.body;
+    const newBear = new Bear({species, latinName});
+    newBear.save()
+      .then(savedBear => {
+        res.status(201).json(savedBear);
+      })
+      .catch(err => {
+        res.status(422).json({error: err});
+      });
   });
 
 router
