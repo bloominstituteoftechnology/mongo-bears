9259660c07abbe60fec5921c9884a3e98ef7690d
Derrick-Mei <derrick.mei.169@gmail.com>
2018-06-04 16:27:22 -0500

    post

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-        node: true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
     },
-    extends: "eslint:recommended",
-    parserOptions: {
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-            jsx: true
-        },
-        sourceType: "module"
-    },
-    plugins: ["react"],
-    rules: {
-        indent: ["error", 2],
-        "linebreak-style": ["error", "windows"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"]
-    }
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "no-console": 0,
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
 };
--- bears/bearController.js
+++ bears/bearController.js
@@ -11,10 +11,33 @@ router
       .then(bearsArray => {
         res.status(200).json(bearsArray);
       })
-      .catch(() => res.status(500).json({ error: "Error fetching bears" }));
+      .catch(() =>
+        res
+          .status(500)
+          .json({ error: "The bear information could not be retrieved." })
+      );
   })
   .post((req, res) => {
-    res.status(201).json({ status: "please implement POST functionality" });
+    const { species, latinName } = req.body;
+    if (!species || !latinName) {
+      res.status(400).json({
+        errorMessage: "Please provide both species and latinName for the bear."
+      });
+    }
+    const newBear = new Bear({ species, latinName });
+    newBear
+      .save() // this will `insert` a document into the Bear collection
+      .then(savedBear => {
+        //console.log("req.body: ", req.body);
+        //console.log("savedBear,:", savedBear);
+        res.status(201).json(savedBear);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the bear to the database"
+        });
+      });
   });
 
 router
