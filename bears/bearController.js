48e8d27e27a292e0cc626447c929f536fb03e008
bbenefield89 <bsquared18@gmail.com>
2018-06-04 15:48:36 -0400

    feat(bearSchema): add new bear schema and model feat(bearController): `GET:/` returns list of all bears feat(bearController): `POST:/` adds new bear to db

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
+const BearModel = require('../bearsSchema');
 
 router
   .route('/')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
+    BearModel.find({}, (err, dbRes) => {
+      if (err) return res.status(500).json({ err });
+      res.json(dbRes);
+    });
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+    const { species, latinName } = req.body;
+    const bear = new BearModel({ species, latinName });
+    BearModel.find({ species }, (err, dbRes) => {
+      if (err) return res.status(500).json({ err: 'Error' });
+      if (dbRes.length > 0) return res.status(422).json({ err: 'This bears already here!' });
+      bear.save((err, dbRes) => {
+        if (err) {
+          if (err.code === 11000) return res.status(422).json({ err: 'Please choose a different latin name' });
+          return res.status(500).json({err})
+        }
+        res.json(dbRes);
+      });
+    });
   });
 
 router
