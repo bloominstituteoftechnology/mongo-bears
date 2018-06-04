fc66254ba07a780dc6cb1c51cd8ba3807a937c99
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-04 13:00:14 -0700

    controller and model initialized for home route

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
+const Bear = require('./bearModel');
 
 router
   .route('/')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
+    Bear.find()
+      .then(bears => {
+        res.status(200).json({ route: '/api/bears/' });
+      })
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+    const { species, latinName } = req.body;
+    const newBear = new Bear({ species, latinName });
+    newBear.save() //inserts a document into the Bear collection
+           .then(savedBear => {
+             res.status(201).json(savedBear);
+           })
+           .catch(err => {
+             res.status(422).json({ error: err});
+           })
   });
 
 router
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const bearController = require('./bears/bearController');
+//we pull in out routes as controller
 
 const server = express();
 
@@ -17,6 +19,19 @@ server.get('/', function(req, res) {
 server.use('/api/bears', bearController);
 
 const port = process.env.PORT || 5000;
+
+mongoose.Promise = global.Promise; // configure the mongoose promise system to use Native JS Promises
+
+mongoose.connect(
+  "mongodb://localhost/dbBears",
+  {},
+  err => {
+    // declare where we're going to connect this is the equivilent of using `use dbBears` in the mongo shell
+    if (err) console.log(err);
+    console.log("Mongoose connected us to our DB");
+  }
+);
+
 server.listen(port, () => {
   console.log(`\n=== API running on http://localhost:${port} ===\n`);
 });
