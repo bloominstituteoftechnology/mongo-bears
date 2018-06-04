c75a1578f738e21bbd936877ae9c334b54c6dc02
Derrick-Mei <derrick.mei.169@gmail.com>
2018-06-04 15:56:23 -0500

    get endpoint

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,9 +1,17 @@
 const router = require("express").Router();
 
+const Bear = require("./bearModel");
+
 router
   .route("/")
   .get((req, res) => {
-    res.status(200).json({ route: "/api/bears/" });
+    //console.log("\nYou got into /api/bears/\n");
+    //res.status(200).json({ route: "/api/bears/" });
+    Bear.find()
+      .then(bearsArray => {
+        res.status(200).json(bearsArray);
+      })
+      .catch(() => res.status(500).json({ error: "Error fetching bears" }));
   })
   .post((req, res) => {
     res.status(201).json({ status: "please implement POST functionality" });
