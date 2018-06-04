c67813fbbc3bc057efa126e0b6f6a78ebba445ae
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-04 15:56:09 -0400

    - implemented CRUD

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,24 +1,55 @@
 const router = require('express').Router();
 
+const Bear = require('./bearModel');
+
 router
   .route('/')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
+    Bear.find() // This will find ALL resources at that model.
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err => res.status(500).json({ error: 'Error fetching bears' }));
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+   const {species, latinName} = req.body;
+   const newBear = new Bear({species, latinName});
+    newBear.save().then(savedBear => {
+      res.status(201).json(savedBear)
+    })
+    .catch(err => {
+      res.status(422).json({error: err})
+   })
   });
 
 router
   .route('/:id')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' + req.params.id });
+   const {id} = req.params;
+   Bear.findById(id).then(foundBear => {
+     res.status(200).json(foundBear)
+   })
+   .catch(err => {
+     res.staus(404).json({error: "No bear with that ID in DB"})
+   });
   })
   .delete((req, res) => {
-    res.status(200).json({ status: 'please implement DELETE functionality' });
+    const {id} = req.params;
+    Bear.findOneAndRemove(id).then(removeBear => {
+      res.status(200).json(removeBear)
+    })
   })
   .put((req, res) => {
-    res.status(200).json({ status: 'please implement PUT functionality' });
+    const {id} = req.params;
+    const {body} = req.body;
+   Bear.findByIdAndUpdat(id,{
+     $set: {
+       species: body,
+       latinName: body
+     }
+   }).then((result) => {
+     console.log(result)
+   })
   });
 
 module.exports = router;
--- bears/bearModel.js
+++ bears/bearModel.js
@@ -1,23 +1,24 @@
-// model for bears
+// create a model for our bears.
 const mongoose = require('mongoose');
 
-// Schema
 const BearSchema = new mongoose.Schema({
-    species: {
-        type: String,
-        require: true,
-        unique: true
-    },
-    latinName: {
-        type: String,
-        require: true
-    },
-    createdOn: {
-        type: Date,
-        default: Date.now()
-    }
+  // declare a schema. Your documents will 'look like this'
+  species: {
+    type: String, // There are many types in Mongoose that we'll learn about this week
+    required: true, // required is a validator. It tells us that this field is required
+    unique: true // unique is also a validator. It tells us this field can only exist once
+  },
+  latinName: {
+    type: String,
+    require: true
+  },
+  createOn: {
+    type: Date, // Date is a nother type in Mongoose.
+    default: Date.now()
+  }
 });
 
-const bearsModel = mongoose.model('Bear', BearSchema)
+const bearsModel = mongoose.model('Bear', BearSchema); // this is where we declare this as a model.
+// by passing our BearSchema to this model we decalre that it will be a collection in our DB
 
-module.export = bearsModel;
+module.exports = bearsModel;
