f9c3e90ff63d986b05ac533f23044c7acd16aae0
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-04 14:45:57 -0500

    Project initialized and Mongoose connected to DB

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-const bearController = require('./bears/bearController');
+const bearController = require('./bears/bearController');// pull in our Routes as controller
 
 const server = express();
 
-server.use(helmet());
+server.use(helmet());// helment adds a layer of security to our API
 server.use(cors());
 server.use(express.json());
 
@@ -14,7 +15,13 @@ server.get('/', function(req, res) {
   res.status(200).json({ api: 'running' });
 });
 
-server.use('/api/bears', bearController);
+server.use('/api/bears', bearController);// this is where we register our routes. EVERYthing on that bearControler will link up to the address of 'api/bears'
+
+mongoose.Promise = global.Promise;// configure the mongoose promise to use Native JS Promises
+mongoose.connect('mongodb://localhost/dbBears', {}, err => {
+  if (err) console.log(err);
+  console.log('Mongoose connected us to our DB');
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
