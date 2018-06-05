29c088c12308a73bb1031fad63b66767347a1b51
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-06 02:30:02 +0700

    made small changes to error messages and commented out some console.logs

--- bears/bearController.js
+++ bears/bearController.js
@@ -16,11 +16,12 @@ router
     const newBear = new Bear({ species, latinName });
     if (!species || !latinName) {
       res.status(400).json({ error: "Please provide both species and latinName for the bear." });
+      return;
     };
     newBear
       .save()
       .then(savedBear => {
-        res.status(201).json(savedBear);
+        res.status(201).json({ success: "Bear Saved Successfully.", resource: savedBear });
       })
       .catch(err => {
         res.status(500).json({ error: "There was an error while saving the bear to the database" });
@@ -32,17 +33,17 @@ router
   .get((req, res) => {
     const { id } = req.params;
     if (id.length !== 24) {
-      res.status(400).send({ error: "The ID entered is too short or too long in length." });
+      res.status(400).send({ error: `${id} is too short or too long in length.` });
       return;
     }
     Bear.findById(id)
       .then(bear => {
-        console.log(bear);
+        // console.log(bear);
         if (bear === null) {
-          res.status(404).json({ error: "Bear cannot be found with given ID." });
+          res.status(404).json({ error: `Bear cannot be found with given ID of ${id}.` });
           return;
         }
-        console.log(bear.id.length);
+        // console.log(bear.id.length);
         res.status(200).json(bear);
       })
       .catch(err => {
@@ -52,16 +53,16 @@ router
   .delete((req, res) => {
     const { id } = req.params;
     if (id.length !== 24) {
-      res.status(400).send({ error: "The ID entered is too short or too long in length." });
+      res.status(400).send({ error: `${id} is too short or too long in length.` });
       return;
     }
     Bear.findByIdAndRemove(id)
       .then(bear => {
         if (bear === null) {
-          res.status(404).json({ error: "Bear cannot be found with given ID." });
+          res.status(404).json({ error: `Bear cannot be found with given ID of ${id}.` });
           return;
         }
-        res.json(bear);
+        res.status(200).json({ success: "Bear Removed From Population", resource: bear });
       })
       .catch(err => {
         res.status(500).json({ error: "Something went terribly wrong!" });
@@ -72,21 +73,20 @@ router
     const { species, latinName } = req.body;
     const updatedBear = { species, latinName };
     if (id.length !== 24) {
-      res.status(400).send({ error: "The ID entered is too short or too long in length." });
+      res.status(400).send({ error: `${id} is too short or too long in length.` });
       return;
-    } else {
-      Bear.findByIdAndUpdate(id, updatedBear)
-        .then(update => {
-          if (update === null) {
-            res.status(404).send({ error: "Bear cannot be found with given ID." });
-            return;
-          }
-          res.json(update);
-        })
-        .catch(err => {
-          res.status(500).json({ error: "Something went terribly wrong!" });
-        });
-    };
+    }
+    Bear.findByIdAndUpdate(id, updatedBear)
+      .then(update => {
+        if (update === null) {
+          res.status(404).send({ error: `Bear cannot be found with given ID of ${id}.` });
+          return;
+        }
+        res.status(200).json({ success: "Bear Updated", resource: update });
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Something went terribly wrong!" });
+      });
   });
 
 module.exports = router;
