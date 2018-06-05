d9fa997dcfe3c100fa4750568296e72c9cf5a372
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-05 08:02:58 +0800

    Configured server.js, bearController.js, and bearModel.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// Pseudo code here
-
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const bearController = require('./bears/bearController');
 
@@ -18,7 +17,14 @@ server.get('/', function(req, res) {
 
 server.use('/api/bears', bearController);
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/dbBears', {}, err => {
+  if (err) console.log(err);
+  console.log('Mongoose connected us to our DB.');
+});
+
 const port = process.env.PORT || 5000;
+
 server.listen(port, () => {
   console.log(`\n=== API running on http://localhost:${port} ===\n`);
 });
