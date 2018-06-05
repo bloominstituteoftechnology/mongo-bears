df160dba55c407638c9951f9e92cfd4c659e25a1
A99 <A99@99s-macbook-pro.home>
2018-06-05 02:06:24 +0200

    Compiling & Serving

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+
 const mongoose = require('mongoose');
 const bearController = require('./bears/bearController');
 const server = express();
+const port = 5000;
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use('/api/bears', bearController);
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:5000/beardb', {}, err => {
-  if(err) console.log(err);
-  console.log(`Mongoose is connected to our DB.`);
-});
-
 server.get('/', function(req, res) {
   res.status(200).json({ api: 'running' });
 });
 
-server.use('/api/bears', bearController);
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1/', {}, err => {
+  if(err) console.log(err);
+  console.log(`Mongoose is connected to our DB.`); 
+});
 
-const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n=== API running on http://localhost:${port} ===\n`);
 });
