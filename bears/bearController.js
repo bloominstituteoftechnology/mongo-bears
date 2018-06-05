2e8ddf5775ddb3e0d46be282d8f41512f60fe816
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-05 09:14:32 +0800

    Configured API. It can now successfully perform CRUD operations on endpoints. All passed test in Postman.

--- bears/bearController.js
+++ bears/bearController.js
@@ -37,10 +37,34 @@ router
       });
   })
   .delete((req, res) => {
-    res.status(200).json({ status: 'Please implement DELETE functionality' });
+    const { id } = req.params;
+    Bear.findByIdAndRemove(id)
+      .then(response => {
+        if (response == 0) {
+          res.status(404).json({ error: 'The bear with the specified ID does not exist.' });
+          return;
+        }
+        res.json({ success: 'Bear removed from DB.' });
+      })
+      .catch(err => 
+        res.status(500).json({ error: 'The bear could not be removed.' })
+      );
   })
   .put((req, res) => {
-    res.status(200).json({ status: 'Please implement PUT functionality' });
-  });
+    const { id } = req.params;
+    const { species, latinName } = req.body;
+    const updatedBear = { species, latinName };
+    if (!species || !latinName) {
+      res.status(404).json({ error: 'The bear with the specified ID does not exist.' })
+      return;
+    }
+    Bear.findByIdAndUpdate(id, updatedBear)
+      .then(response => {
+        res.json(response);
+      })
+      .catch(err => 
+        res.status(500).json({ error: 'The bear information could not be modified.'})
+      );
+});
 
 module.exports = router;
