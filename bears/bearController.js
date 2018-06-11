ca1a882633c6a0818f7d277b9bcde05955f677dd
maribelcuales <maribelcuales@gmail.com>
2018-06-11 10:34:14 +0800

    fixed some of the codes and edited the error messages

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
+const router = require('express').Router();  // declare that all routes for this address will be found on this router
 
-const Bear = require('./bearModel');
+const Bear = require('./bearModel');  // pull in our Bear model
 
 router
   .route('/')
   .get((req, res) => {
-    Bear.find()
+    Bear.find()  // This will find ALL resources at that model.
       .then(bears => {
         res.status(200).json(bears);
       })
@@ -19,13 +19,13 @@ router
       return;
     }
     newBear
-      .save()
-      .then(response => {
-        console.log(response);
+      .save()   // this will `insert` a document into the Bear collection
+      .then(savedBear => {
+        console.log(savedBear);
           res.status(201).json({ status: 'Successfully added!' });    
       })
       .catch(err => {
-        res.status(500).json({ error: 'Error while saving the bear to database.' });
+        res.status(500).json({ error: error.message });
       });
 });
 
@@ -33,23 +33,31 @@ router
   .route('/:id')
   .get((req, res) => {
     const { id } = req.params;
-    Bear.findById(id)
-      .then(response => {
+    Bear.findById(id)   // find a specific resource in a collection by ID
+      .then(foundBear => {
+        console.log(foundBear);
+        if(foundBear === null) {
+          res.status(404).json({ error: 'No bear by that id in the databse.'});
+          return;
+        }
         res.status(200).json({ success: 'Bear found.' });
       })
       .catch(err => {
         res.status(404).json({ error: 'The bear with the specified ID does not exist.'});
       });
-      })
+    })
     .delete((req, res) => {
       const { id } = req.params;
     Bear.findByIdAndRemove(id)
-      .then(response => {
-        if (response == 0) {
+      .then(removedBear => {
+        console.log(removedBear); 
+        if (removedBear === null) {
+          // IF there is no bear by that id, then mongo won't throw an error, rather give us back a null object
+          // MAKE SURE you handle this null object!!!
           res.status(404).json({ error: 'The bear with the specified ID does not exist.' });
           return;
         }
-        res.json({ success: 'Bear removed from database.' });
+        res.json({ success: `Bear removed.`, resource: removedBear });
       })
       .catch(err => 
         res.status(500).json({ error: 'The bear could not be removed.' })
@@ -57,19 +65,23 @@ router
   })  
     .put((req, res) => {
       const { id } = req.params;
-    const { species, latinName } = req.body;
-    const updatedBear = { species, latinName };
-    if (!species || !latinName) {
-      res.status(404).json({ error: 'The bear with the specified ID does not exist.' })
-      return;
-    }
-    Bear.findByIdAndUpdate(id, updatedBear)
-      .then(response => {
-        res.json(response);
+      const updates = ({ species, latinName } = req.body);      
+      if (!species || !latinName) {
+        res.status(404).json({ error: 'The bear with the specified ID does not exist.' })
+        return;
+      }
+      Bear.findByIdAndUpdate(id, updates, { new: true})  // new: true will give you the updated resource, not the previous one
+        .then(bearUpdated => {
+          if (bearUpdated === null) {
+            // IF there is no bear by that id, then mongo won't throw an error, rather give us back a null object
+            // MAKE SURE you handle this null object!!!
+            res.status(404).json({ error: 'No bear by that ID found in database.' }); 
+            return; 
+          }
+          res.json({ success: 'Updated the bear.', resource: bearUpdated });
       })
-      .catch(err => 
-        res.status(500).json({ error: 'The bear information could not be modified.'})
-      );
+          .catch(err => 
+            res.status(500).json({ error: error.message }));
 });
 
 module.exports = router;
--- bears/bearModel.js
+++ bears/bearModel.js
@@ -1,5 +1,7 @@
+// create a model for our bears.
 const mongoose = require('mongoose');
 
+// Schema
 // {
 //     species: 'Grizzly Bear',
 //     latinName: 'Ursus arctos horribilis',
@@ -7,21 +9,23 @@ const mongoose = require('mongoose');
 // }
 
 const BearSchema = new mongoose.Schema({
+    // declare a schema. Your documents will 'look like this'
     species: {
-        type: String,
-        required: true,
-        unique: true
+        type: String,  // There are many types in Mongoose 
+        required: true,  // required is a validator. It tells us that this field is required
+        unique: true   // unique is also a validator. It tells us this field can only exist once
     },
     latinName: {
         type: String,
         required: true
     },
     createOn: {
-        type: Date,
+        type: Date,   // Date is a nother type in Mongoose.
         default: Date.now()
     }
 });
 
-const bearsModel = mongoose.model('Bear', BearSchema);
+const bearsModel = mongoose.model('Bear', BearSchema);   // this is where we declare this as a model.
+// by passing our BearSchema to this model we decalre that it will be a collection in our DB
 
 module.exports = bearsModel;
