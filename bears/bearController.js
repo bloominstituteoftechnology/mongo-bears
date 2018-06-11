6c11f9947fb6ff80637eaaf475d6be53619df796
Usman Javed <ujaved931@gmail.com>
2018-06-12 00:36:32 +0500

    finished mongodb

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,24 +1,70 @@
 const router = require('express').Router();
 
+const Bear = require('./bearModel');
+
 router
   .route('/')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
+    Bear.find()
+      .then(bears => {
+        res.status(200).json(bears);
+      }).catch(err => res.status(500).json({ error: 'Error fetching bears' }));
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+    const { species, latinName } = req.body;
+    const newBear = new Bear({ species, latinName });
+    newBear.save()
+      .then(savedBear => {
+        res.status(201).json(savedBear);
+      })
+      .catch(err => {
+        res.status(500).json({ error: err });
+      })
   });
 
 router
   .route('/:id')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' + req.params.id });
+    const { id } = req.params;
+    Bear.findById(id)
+      .then(foundbear => {
+        res.status(200).json({ foundbear });
+      })
+      .catch(err => {
+        res.status(404).json({ error: "does not exist" })
+      })
+
   })
   .delete((req, res) => {
-    res.status(200).json({ status: 'please implement DELETE functionality' });
+    const { id } = req.params;
+    Bear.findByIdAndRemove(id)
+      .then(bearDelete => {
+        if (bearDelete === null) {
+          res.status(404).json({ error: "the bear with this id does not exist" })
+          return;
+        }
+        res.json({ sucess: 'Bear removed', resource: bearDelete });
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'The bear cannot be removed' })
+      })
   })
   .put((req, res) => {
-    res.status(200).json({ status: 'please implement PUT functionality' });
+    const { id } = req.params;
+    const updates = ({ species, latinName } = req.body);
+    if (!species || !latinName) {
+      res.status(404).json({ error: 'The bear with the specified ID does not exist.' })
+      return;
+    }
+    Bear.findByIdAndUpdate(id, updates, { new: true }) 
+      .then(bearUpdated => {
+        if (bearUpdated === null) {
+          res.status(404).json({ error: 'No bear by that ID found in database.' });
+          return;
+        }
+        res.json({ success: 'Updated the bear.', resource: bearUpdated });
+      })
+      .catch(err =>
+        res.status(500).json({ error: error.message }));
   });
-
 module.exports = router;
