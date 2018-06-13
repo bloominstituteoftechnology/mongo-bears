73934911d8bd76f95e58526d0fe0dd2b81b5460e
april <martinezapril56@yahoo.com>
2018-06-13 08:56:51 -0400

    got my server side on bears to work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require( 'express' );
 const helmet = require( 'helmet' );
 const cors = require( 'cors' );
-
+const bearController = require( './bears/bearController' );
 const mongoose = require( 'mongoose' );
 
 const server = express();
@@ -17,11 +17,12 @@ server.get( '/', ( req, res ) =>
 
 
 
-const port = process.env.PORT || 5555;
+server.use( '/api/bears', bearController );
 mongoose.Promise = global.Promise;
-mongoose.connect( 'mongodb://localhost/dbfriends', {}, err =>
+mongoose.connect( 'mongodb://localhost/dbbears', {}, err =>
 {
   if ( err ) console.log( err );
   console.log( 'mongoose connected to our DB' );
 } );
+const port = process.env.PORT || 5000;
 server.listen( port, () => console.log( `\n=== API up on port: ${ port } ===\n` ) );
